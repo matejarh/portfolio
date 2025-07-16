906791291bc9bf2f018d6c216a28cfc504c2c03e
Matej Arh <matej.arh@gmail.com>
2025-07-16 17:24:23 +0200

    add contact email info@web3-solutions.eu

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,6 +2,7 @@
   <section class="p-8 max-w-xl mx-auto text-center">
     <h2 class="text-3xl font-bold mb-4" data-aos="fade-up">Contact Me</h2>
     <p class="mb-2" data-aos="fade-up" data-aos-delay="200">📧 <a class="text-blue-500 hover:underline" href="mailto:matej.arh@gmail.com">matej.arh@gmail.com</a></p>
-    <p  data-aos="fade-up" data-aos-delay="400">💼 <a class="text-blue-500 hover:underline" href="https://www.linkedin.com/in/matej-arh-48902026/" target="_blank">LinkedIn</a></p>
+    <p class="mb-2" data-aos="fade-up" data-aos-delay="400">📧 <a class="text-blue-500 hover:underline" href="mailto:info@web3-solutions.eu">info@web3-solutions.eu</a></p>
+    <p  data-aos="fade-up" data-aos-delay="600">💼 <a class="text-blue-500 hover:underline" href="https://www.linkedin.com/in/matej-arh-48902026/" target="_blank">LinkedIn</a></p>
   </section>
 </template>
